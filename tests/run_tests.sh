e64ba447af469c6229f85d331b5521d3729eeeca
Vincent Mihalkovic <vmihalko@redhat.com>
2019-11-12 18:13:09 +0100

    test script simplified

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for test_file in $(ls *input.txt)
+
+for test_file in *input.txt
 do
 		cat $test_file | ./../formatCBMCOutput.py | diff -u $(echo $test_file | cut -d'_' -f1)*_output.txt -
 done	
