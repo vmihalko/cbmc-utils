a7139fd0a730e30010f435abfd273d1886e84326
Vincent Mihalkovic <vmihalko@redhat.com>
2019-11-19 15:03:28 +0100

    path to script changed

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,5 +2,5 @@
 
 for test_file in *input.txt
 do
-		cat $test_file | ../formatCBMCOutput.py | diff -u $(echo $test_file | cut -d'_' -f1)*_output.txt -
+		cat $test_file | formatCBMCOutput | diff -u $(echo $test_file | cut -d'_' -f1)*_output.txt -
 done	
